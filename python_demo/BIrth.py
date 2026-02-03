055861fb8cb478b9c7000a71a2f89670c9592690
Su457 <2083267493@qq.com>
2026-02-03 16:57:27 +0800

    1

--- python_demo/BIrth.py
+++ python_demo/BIrth.py
@@ -1,5 +1,8 @@
-birth_year = input('Brith year: ')
-print(type(birth_year))
-age = 2026-int(birth_year)
-print(type(age))
-print(age)
+try:
+    birth_year = input('Brith year: ')
+    print(type(birth_year))
+    age = 2026-int(birth_year)
+    print(type(age))
+    print(f'Your age is: {age}')
+except ValueError:
+    print("Please enter a valid number for birth year.")
